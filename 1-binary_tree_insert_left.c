21493f1a862558a7c6818cc22edb9f831ef008b9
alereginensi <reginensia@gmail.com>
2022-07-16 14:51:57 -0700

    das

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left -  inserts a node as the left-child of another node
+ * @value: value
+ * @parent: parent node
+ * Return: result
+**/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *result = malloc(sizeof(binary_tree_t));
+
+	if (result != NULL)
+	{
+		result->parent = parent;
+		result->left = parent;
+		result->value = parent;
+	}
+	return (result);
+}
