11949fad4173f911fa13f8b7d048d7b1523992ac
MauriPercovich <mauriciopercovich22@gmail.com>
2022-07-16 16:37:50 -0700

    comments

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 /**
- *
- *
+ * binary_tree_is_root - checks if node is root
+ * @node: Node to check
+ * Return: 0 or 1
 **/
 int binary_tree_is_root(const binary_tree_t *node)
 {
