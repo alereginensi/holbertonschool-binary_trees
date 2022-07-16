f2c9f97d1f27553afc89373a51a1c79ce838ebfe
MauriPercovich <mauriciopercovich22@gmail.com>
2022-07-16 15:30:22 -0700

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,8 +13,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (result != NULL)
 	{
 		result->parent = parent;
-		result->left = parent;
-		result->value = parent;
+		result->left = NULL;
+		result->right = NULL;
+		result->n = value;
+		if (parent->left != NULL)
+			parent->left = result;
 	}
 	return (result);
 }
