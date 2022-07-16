1824737262de06ffa35b9c0b20b05f0ec02fec5b
MauriPercovich <mauriciopercovich22@gmail.com>
2022-07-16 14:24:35 -0700

    task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ *
+ *
+**/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	value = malloc(sizeof(binary_tree_t));
+	if (value != NULL)
+	{
+		//value->parent = NULL;//
+		value->n = n;
+	}
+	return (value);
+}
