d4e07965adb80e91d6208e15253340a02bac833d
alereginensi <reginensia@gmail.com>
2022-07-17 17:47:27 -0700

    task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,13 +6,13 @@
 **/
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->parent == NULL)
-		return (0);
-
-	size_t depth = 1 + binary_tree_depth(tree->parent);
+	if (tree->parent)
+		depth = 1 + binary_tree_depth(tree->parent);
 
 	return (depth);
 }
