43fc1196622acce9835b7d24dcd9bfb45db3bb21
MauriPercovich <mauriciopercovich22@gmail.com>
2022-07-18 14:09:02 -0700

    14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - function that measures the balance factor of a binary tree
+ * @tree: Tree
+ * Return: balance factor of a binary tree
+**/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+
+
+}
