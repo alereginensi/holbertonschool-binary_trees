457549edd123f93d695ecfd4dd4e1c24d9f898dc
alereginensi <reginensia@gmail.com>
2022-07-17 17:31:15 -0700

    task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,16 +7,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->right == NULL && node->left == NULL)
+	if (node != NULL && node->right == NULL && node->left == NULL)
 	{
 		return (1);
 	}
-	else if (node == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
